c4e5b3a1b51d369901f634517e625b4dffa16721
Richard Joseph Omega <richardjoseph.omega@gmail.com>
2026-01-31 21:03:35 +0800

    pass bytes instead of temporary file

--- services/stt/stt/service_impl.py
+++ services/stt/stt/service_impl.py
@@ -1,4 +1,3 @@
-# stt/service_impl.py
 import tempfile
 from stt import transcriber_pb2
 from stt import transcriber_pb2_grpc
@@ -8,14 +7,9 @@ class TranscriptionService(transcriber_pb2_grpc.TranscriptionServiceServicer):
         self.pipe = model_pipeline
 
     def Transcribe(self, request, context):
-        # Save audio bytes to a temp file
-        with tempfile.NamedTemporaryFile(suffix=".wav", delete=False) as f:
-            f.write(request.audio)
-            audio_path = f.name
-
         # Whisper inference
         result = self.pipe(
-            audio_path,
+            request.audio,
             generate_kwargs={
                 "language": request.language or None,
             }
